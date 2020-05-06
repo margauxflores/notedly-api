091974bf030469aa4d6bbbdd2458fa4365644ffc
Margaux Flores <margaux.flores@bitflyer.com>
2020-05-06 18:05:19 +0900

    Remove demo notes

--- api/src/index.js
+++ api/src/index.js
@@ -34,9 +34,3 @@ app.listen({ port }, () =>
     `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
   )
 );
-
-let notes = [
-  { id: '1', content: 'This is a note', author: 'Adam Scott' },
-  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
-  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
-];
